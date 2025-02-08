9ac6f9033c2e9b1c646309f6f008d8531f955b49
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-02-09 00:58:42 +0100

    update portfolio

--- src/const/const.js
+++ src/const/const.js
@@ -1,18 +1,18 @@
 export const studies = [
   {
-    title: "Técnico en desarrollo de software",
-    corporation: "SENA - Montería (Córdoba) ",
-    date: "02/2019 – 12/2019",
+    title: "Desarrollo Web y Programación",
+    corporation: "Ironhack (Spain) ",
+    date: "10/2024 – 12/2024",
   },
   {
-    title: "Desarrollador Front-end React Js",
-    corporation: "Alura Latam - Remoto",
-    date: "04/2023 – 09/2023",
+    title: "Grado Superior en Desarrollo de Aplicaciones Multiplataforma (DAM)",
+    corporation: "Ilerna",
+    date: "09/2023 – 06/2025",
   },
   {
-    title: "Curso profesional de React Js",
-    corporation: "Código facilito - Remoto",
-    date: "04/2023 – 04/2023",
+    title: "Grado en gestión de recursos humanos/Administración de personal",
+    corporation: "Universidad de Valencia",
+    date: "09/2011 – 06/2015",
   },
 ];
 
@@ -20,24 +20,26 @@ export const IconKeys = {
   html: "html",
   css: "css",
   javaScript: "javaScript",
-  react: "react",
-  astro: "astro",
-  materialUi: "materialUi",
-  nextjs: "nextjs",
-  redux: "redux",
-  styled: "styled",
-  tailwind: "tailwind",
-  radix: "radix",
-  reactQuery: "reactQuery",
   typeScript: "typeScript",
-  zustand: "zustand",
+  react: "react",
+  express: "express",
+  astro: "astro",
+  nextjs: "nextjs",
+  jest: "jest",
   git: "git",
   github: "github",
-  stripe: "stripe",
-  shadcn: "shadcn",
+  php: "php",
   nodejs: "nodejs",
   mongodb: "mongodb",
   vite: "vite",
+  jira: "jira",
+  figma: "figma",
+  miro: "miro",
+  postman: "postman",
+  bruno: "bruno",
+  notion: "notion",
+  trello: "trello",
+
   // here add more icon keys
 };
 
@@ -59,12 +61,12 @@ export const skills = [
     icon: IconKeys.astro,
   },
   {
-    title: "Tailwindcss",
-    icon: IconKeys.tailwind,
-  },
-  {
     title: "React Js",
     icon: IconKeys.react,
+  },
+  {
+    title: "Jest",
+    icon: IconKeys.jest,
   },
   {
     title: "Next Js",
@@ -74,23 +76,9 @@ export const skills = [
     title: "Git",
     icon: IconKeys.git,
   },
-
   {
     title: "Github",
     icon: IconKeys.github,
-  },
-  {
-    title: "Material UI",
-    icon: IconKeys.materialUi,
-  },
-
-  {
-    title: "Redux Js",
-    icon: IconKeys.redux,
-  },
-  {
-    title: "Styled Components",
-    icon: IconKeys.styled,
   },
   {
     title: "Node Js",
@@ -101,81 +89,58 @@ export const skills = [
     icon: IconKeys.typeScript,
   },
   {
-    title: "Radix UI",
-    icon: IconKeys.radix,
-  },
-
-  {
-    title: "React Query",
-    icon: IconKeys.reactQuery,
+    title: "PHP",
+    icon: IconKeys.php,
   },
   {
     title: "Vite",
     icon: IconKeys.vite,
   },
   {
-    title: "Zustand",
-    icon: IconKeys.zustand,
-  },
-
-  {
-    title: "Shadcn UI",
-    icon: IconKeys.shadcn,
-  },
-  {
     title: "Mongo Db",
     icon: IconKeys.mongodb,
   },
+  {
+    title: "Express",
+    icon: IconKeys.express,
+  },
+  {
+    title: "Jira",
+    icon: IconKeys.jira,
+  },
+  {
+    title: "Trello",
+    icon: IconKeys.trello,
+  },
+  {
+    title: "Figma",
+    icon: IconKeys.figma,
+  },
+  {
+    title: "Miro",
+    icon: IconKeys.miro,
+  },
+  {
+    title: "Postman",
+    icon: IconKeys.postman,
+  },
+  {
+    title: "Bruno",
+    icon: IconKeys.bruno,
+  },
+  {
+    title: "Notion",
+    icon: IconKeys.notion,
+  },
+
 ];
 
 export const projects = [
   {
-    img: "../../../img/ecommerce-stripe-2.webp",
-    title: "Ecommerce Stripe",
-    link: "https://ecommerce-stripe-763m.vercel.app/",
-    description: `Proyecto FullStack de un ecommerce con pasarela de pagos stripe integrada, destalles de el product etc.  `,
-    technologies: [
-      {
-        icon: IconKeys.typeScript,
-      },
-      {
-        icon: IconKeys.nextjs,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-      {
-        icon: IconKeys.mongodb,
-      },
-      {
-        icon: IconKeys.stripe,
-      },
-    ],
-  },
-  {
-    img: "../../../img/nexanime.webp",
-    title: "NexAnime",
-    link: "https://nexanime.vercel.app",
-    description: `plataforma de anime para ver los animes en tendencia , más valorados , estadísticas.`,
-    technologies: [
-      {
-        icon: IconKeys.typeScript,
-      },
-      {
-        icon: IconKeys.nextjs,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
-  {
-    img: "../../../img/cyberpunk.webp",
-    title: "cyberpunk",
-    link: "https://preeminent-liger-266356.netlify.app/",
-    description: `este proyecto se trata de un clon de la pagína de cyberpunk , donde
-      el ususario puede ver los tailer de él juegos y también los
-      episodios de la serie....`,
+    img: "../../../img/PickUp.png",
+    title: "PickUp",
+    link: "https://lareview2.netlify.app/",
+    description: `Aplicación web para compartir críticas de cine y unirse a comunidades de cinéfilos. Incluye un sistema de autenticación de usuarios y gestión dinámica de bases de datos con MongoDB.`,
     technologies: [
       {
         icon: IconKeys.html,
@@ -186,18 +151,28 @@ export const projects = [
       {
         icon: IconKeys.javaScript,
       },
+      {
+        icon: IconKeys.mongodb,
+      },
+      {
+        icon: IconKeys.express,
+      },
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.nodejs,
+      }
     ],
   },
   {
-    img: "../../../img/store-games.webp",
-    title: "Store games",
-    link: "https://store-games-site.netlify.app/",
-    description: `Simulando una página de juegos , consumiendo la API de rawg api
-      donde los usuarios pueden buscar videojuegos, ver sus detalles como
-      creadores , calificaciones,tiendas donde comprar.....`,
+    img: "../../../img/LaPremier.png",
+    title: "LaPremier",
+    link: "https://lapremiere.netlify.app/",
+    description: `Aplicación web para conocer cines, ver que películas se emiten o estrenan y compartir reseñas de las películas. Incluye un sistema de autenticación de usuarios y gestión dinámica de bases de datos con MongoDB.`,
     technologies: [
       {
-        icon: IconKeys.react,
+        icon: IconKeys.html,
       },
       {
         icon: IconKeys.css,
@@ -206,22 +181,27 @@ export const projects = [
         icon: IconKeys.javaScript,
       },
       {
-        icon: IconKeys.tailwind,
+        icon: IconKeys.mongodb,
       },
       {
-        icon: IconKeys.reactQuery,
+        icon: IconKeys.express,
       },
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.nodejs,
+      }
     ],
   },
   {
-    img: "../../../img/movie.webp",
-    title: "Movie Tv",
-    link: "https://movietvsite.netlify.app/",
-    description: `simulando una página de películas , consumiendo la API de themovietv
-              donde los usuarios pueden buscar películas y ver trailer.....`,
+    img: "../../../img/Solentine.png",
+    title: "Solentine",
+    link: "https://solentine.netlify.app/",
+    description: `Administrador de tareas con autenticación de usuarios y gestión dinámica de bases de datos con MongoDB.`,
     technologies: [
       {
-        icon: IconKeys.react,
+        icon: IconKeys.html,
       },
       {
         icon: IconKeys.css,
@@ -230,99 +210,17 @@ export const projects = [
         icon: IconKeys.javaScript,
       },
       {
-        icon: IconKeys.tailwind,
+        icon: IconKeys.mongodb,
       },
-    ],
-  },
-
-  {
-    img: "../../../img/fit-nation.webp",
-    title: "gym",
-    link: "https://fitnation.netlify.app/",
-    description: `landing page de una gym por ahora es una landing todavia está en
-              desarrollo la idea es que los usuarios puedan comprar productos de
-              el gym`,
-    technologies: [
+      {
+        icon: IconKeys.express,
+      },
       {
         icon: IconKeys.react,
       },
       {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
-  {
-    img: "../../../img/spotify.webp",
-    title: "Spotify-Clone",
-    link: "https://www.linkedin.com/posts/osnaider-mart%C3%ADnez_clon-de-la-p%C3%A1gina-de-spotify-echo-con-react-activity-7119712602342965248-upgL?utm_source=share&utm_medium=member_desktop",
-    description: `Clon de la página de Spotify con autenticación de usuario, reproductor de canciones, etc `,
-    technologies: [
-      {
-        icon: IconKeys.react,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-      {
-        icon: IconKeys.redux,
-      },
-      {
-        icon: IconKeys.reactQuery,
-      },
-    ],
-  },
-
-  {
-    img: "../../../img/landing.webp",
-    title: "Landing Realidad Virtual",
-    link: "https://realityvirtual.netlify.app/",
-    description: `landing page de gafas de realidad virtual donde se muestra el
-              producto y como funciona`,
-    technologies: [
-      {
-        icon: IconKeys.astro,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
-  {
-    img: "../../../img/tasks-manager.webp",
-    title: "Tasks Manager",
-    link: "https://task-manager-ecru-chi.vercel.app/dashboard",
-    description: `Administrador de tareas con drag and drop autenticación con Clerk , shadcn.  "En Desarrollo "`,
-    technologies: [
-      {
-        icon: IconKeys.typeScript,
-      },
-      {
-        icon: IconKeys.nextjs,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-      {
-        icon: IconKeys.zustand,
-      },
-      {
-        icon: IconKeys.shadcn,
-      },
+        icon: IconKeys.nodejs,
+      }
     ],
   },
 ];
