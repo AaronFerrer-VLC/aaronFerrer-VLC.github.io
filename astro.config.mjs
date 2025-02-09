d3ed0672e048b74266daac92bb196eda85fad9d6
Aarón Ferrer <aaronferrerbarbas@gmail.com>
2025-02-09 11:58:47 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
-// If you are not going to use vercel analytics you can delete the analytics
-// If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
-  site: 'https://aaronferrer-vlc.github.io/',
-  base: '/AaronFerrer-VLC.github.io/',
->>>>>>> 978b83e391050441821cef9271337c147194d25e
+  site: "https://aaronferrer-vlc.github.io/",
+  base: "/AaronFerrer-VLC.github.io/", 
   output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
+
 
 //If you don't have vercel the configuration will be empty
 
