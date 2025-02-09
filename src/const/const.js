6964adacaeb83ea74cadc777098fae2f7b801f71
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-02-09 20:12:37 +0100

    new component

--- src/const/const.js
+++ src/const/const.js
@@ -14,7 +14,12 @@ export const studies = [
     corporation: "Universidad de Valencia",
     date: "09/2011 – 06/2015",
   },
-];
+  {
+    title: "Grado Superior en Administración y gestión de empresas",
+    corporation: "IES Serpis",
+    date: "09/2009 - 06/2011",
+  },
+]
 
 export const IconKeys = {
   html: "html",
@@ -137,7 +142,7 @@ export const skills = [
 
 export const projects = [
   {
-    img: "/AaronFerrer-VLC.github.io/img/PickUp.png",
+    img: "../../img/PickUp.png",
     title: "PickUp",
     link: "https://lareview2.netlify.app/",
     description: `Aplicación web para compartir críticas de cine y unirse a comunidades de cinéfilos. Incluye un sistema de autenticación de usuarios y gestión dinámica de bases de datos con MongoDB.`,
@@ -166,7 +171,7 @@ export const projects = [
     ],
   },
   {
-    img: "/AaronFerrer-VLC.github.io/img/LaPremier.png",
+    img: "../../img/LaPremier.png",
     title: "LaPremier",
     link: "https://lapremiere.netlify.app/",
     description: `Aplicación web para conocer cines, ver que películas se emiten o estrenan y compartir reseñas de las películas. Incluye un sistema de autenticación de usuarios y gestión dinámica de bases de datos con MongoDB.`,
@@ -195,7 +200,7 @@ export const projects = [
     ],
   },
   {
-    img: "/AaronFerrer-VLC.github.io/img/Solentine.png",
+    img: "../../img/Solentine.png",
     title: "Solentine",
     link: "https://solentine.netlify.app/",
     description: `Administrador de tareas con autenticación de usuarios y gestión dinámica de bases de datos con MongoDB.`,
@@ -224,3 +229,38 @@ export const projects = [
     ],
   },
 ];
+
+export const certificates = [
+  {
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png',
+    title: 'PHP avanzado',
+    issuer: 'LinkedIn',
+    date: 'feb. 2025',
+    skills: ['PHP'],
+    credential: 'https://www.linkedin.com/learning/certificates/d615d458e27a61598bd4ae0f045bcba0b6aa0825bf78e34aeeb95fcbdd7d6ffb'
+  },
+  {
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png',
+    title: 'PHP esencial',
+    issuer: 'LinkedIn',
+    date: 'feb. 2025',
+    skills: ['PHP'],
+    credential: 'https://www.linkedin.com/learning/certificates/45cd59aa99d08ff173a9add9f50824922ff645aa3af89448c655997c68260ad6'
+  },
+  {
+    logo: 'https://res.cloudinary.com/de0ld2xax/image/upload/v1739124084/descarga_oux27a.png',
+    title: 'MAD WD FT 2024-10-14 ES',
+    issuer: 'Ironhack',
+    date: 'dic. 2024',
+    skills: ['CSS', 'Node.js', 'JavaScript', 'Trabajo en equipo', 'HTML5', 'Express.js', 'React.js'],
+    credential: 'https://drive.google.com/file/d/1Sd3I3Uo1v8QvWNShVIQjdogwiqjAILXI/view'
+  },
+  {
+    logo: 'https://agilar.com/images/logos/agilar-color.svg',
+    title: 'Certified Scrum Product Owner® (SCPO®)',
+    issuer: 'Agilar Spain',
+    date: 'jun. 2021',
+    skills: ['Metodologías Agile y Waterfall', 'Road Map', 'Product Owner', 'Descubrimiento de producto', 'Scrum', 'Desarrollo de software'],
+    credential: 'https://drive.google.com/file/d/1I5_WT9vUVTgRh-WupCQO16FmeSY0oRb9/view'
+  }
+]
