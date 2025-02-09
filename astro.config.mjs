3055a394bc77d47129d2d387aedd643fb0c79337
Aarón Ferrer <aaronferrerbarbas@gmail.com>
2025-02-09 14:20:13 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://aaronferrer-vlc.github.io",
   base: "AaronFerrer-VLC.github.io",
-  output: "server",
+  output: "static",
 });
 
 
