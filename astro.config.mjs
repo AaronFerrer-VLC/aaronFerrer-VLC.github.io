bb6ce5ffab6e49827febac771b99c03ac2677b8b
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-03-12 11:28:03 +0100

    new update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
-  site: 'https://AaronFerrer-VLC.github.io',
-  base: '/AaronFerrer-VLC.github.io/',
+  site: 'https://aaronFerrer-VLC.github.io',
+  base: '/aaronFerrer-VLC.github.io/',
   output: "static", // Change to "static" for GitHub Pages
 })
 
 // If you don't have vercel the configuration will be empty
 
 // ej : export default defineConfig({});
+x
