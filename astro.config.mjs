183f22d110c202a4435557149c4a39104694e855
Aaron Ferrer <aaronferrerbarbas@gmail.com>
2025-03-12 11:56:37 +0100

    change in deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,6 @@ export default defineConfig({
   site: 'https://AaronFerrer-VLC.github.io',
   base: '/AaronFerrer-VLC.github.io/',
   output: "static", // Change to "static" for GitHub Pages
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/passthrough",
-    },
-  },
 })
 
 // If you don't have vercel the configuration will be empty
